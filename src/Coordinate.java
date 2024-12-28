b3d5ad5d1a4978ec99918a9cba6cd456bc4e1294
PaascuZ <123894030+PaascuZ@users.noreply.github.com>
2024-12-28 22:16:20 +0100

    Add files via upload

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -2,11 +2,11 @@ public class Coordinate {
     int row;
     int col;
 
-    Coordinate(){
-    }
-
     Coordinate(int row, int col){
         this.row = row;
         this.col = col;
+    }
+
+    Coordinate(){
     }
 }
